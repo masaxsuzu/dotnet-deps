fe81ac71e6a30fd17f00b52f4a4a59bab20e730b
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-10-05 15:26:35 +0900

    Add command line parser

---
+++ src/Netsoft.Tools.Deps/Arguments.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netsoft.Tools.Deps
+{
+    class Arguments
+    {
+        [Option('s', "solution", Required = true, HelpText = "Solution path to analyze.")]
+        public string SolutionFilePath { get; set; }
+    }
+}
--- src/Netsoft.Tools.Deps/Program.cs
+++ src/Netsoft.Tools.Deps/Program.cs
@@ -11,22 +11,56 @@ using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis.Text;
-
+using CommandLine;
 namespace Netsoft.Tools.Deps
 {
     class Program
     {
-        static async Task Main()
+        static async Task<int> Main(string[] args)
         {
+            var parsed = CommandLine.Parser.Default.ParseArguments<Arguments>(args);
+
+            if (parsed.Tag == ParserResultType.NotParsed)
+            {
+                var notParsed = parsed as NotParsed<Arguments>;
+                
+                foreach (var error in notParsed.Errors)
+                {
+                    if (error.Tag == ErrorType.HelpRequestedError ||
+                        error.Tag == ErrorType.VersionRequestedError)
+                    {
+                        continue;
+                    }
+                    Console.Error.WriteLine(error);
+                }
+
+                return 1;
+            }
+
+            string solutionPath = (parsed as Parsed<Arguments>)
+                .Value
+                .SolutionFilePath;
+
+            if (!File.Exists(solutionPath))
+            {
+                Console.Error.WriteLine($"Not found solution file '{solutionPath}'.");
+                return 2;
+            }
+
             // Attempt to set the version of MSBuild.
             var visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
+
+            if (visualStudioInstances.Length < 0)
+            {
+                Console.Error.WriteLine($"Not found Visual Studio instance.");
+                return 3;
+            }
+
             var instance = visualStudioInstances[0];
 
             MSBuildLocator.RegisterInstance(instance);
             using (var workspace = MSBuildWorkspace.Create())
             {
-                string solutionPath = @"..\..\Netsoft.Tools.Deps.sln";
-
                 var solution = await workspace.OpenSolutionAsync(solutionPath);
 
                 var walker = new SolutionWalker();
@@ -36,6 +70,8 @@ namespace Netsoft.Tools.Deps
                     walker.Walk(solution, progress, progress);
                 }
             }
+
+            return 0;
         }
     }
 }
