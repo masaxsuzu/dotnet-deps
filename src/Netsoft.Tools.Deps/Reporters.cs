71b07515558252d997a310242661f4e80c06ac41
masaxsuzu <masaxsuzu+github@gmail.com>
2020-11-27 23:32:14 +0900

    Add JsonReporter

--- src/Netsoft.Tools.Deps/Reporters.cs
+++ src/Netsoft.Tools.Deps/Reporters.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -62,4 +63,74 @@ namespace Netsoft.Tools.Deps
         }
         #endregion
     }
+
+    public class JsonReporter : IDisposable,
+        IProgress<Project>, 
+        IProgress<(Project, Project)>,
+        IProgress<(Project, object)>
+    {
+        private readonly TextWriter _writer;
+        private readonly List<Project> _projects;
+        public JsonReporter(TextWriter writer)
+        {
+            _writer = writer;
+            _projects = new List<Project>();
+        }
+
+        public void Report(Project value)
+        {
+            _projects.Add(value);
+        }
+
+        public void Report((Project, Project) value)
+        {
+        }
+
+        public void Report((Project, object) value)
+        {
+        }
+
+        private string GetFilePathFromSolution(Project project)
+        {
+            var slnDir = new DirectoryInfo(project.Solution.FilePath);
+            return project.FilePath
+                .Replace(slnDir.Parent.FullName, "")
+                .TrimStart('\\')
+                .Replace("\\", "\\\\");
+        }
+
+        #region IDisposable Support
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _writer.WriteLine("[");
+                    _writer.WriteLine(string.Join(
+                        ",\n",
+                        _projects
+                        .Select(p => $"  {{ \"Name\": \"{p.Name}\", \"AssemblyName\": \"{p.AssemblyName}\", \"FilePathFromSolutionDir\": \"{GetFilePathFromSolution(p)}\" }}")));
+                    _writer.WriteLine("]");
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        // ~DotProgress()
+        // {
+        //   Dispose(false);
+        // }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+
 }
---
+++ tests/Netsoft.Tests.Tools.Deps/TestJsonReporter.cs
@@ -0,0 +1,66 @@
+using Microsoft.CodeAnalysis;
+using Netsoft.Tools.Deps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Netsoft.Tests.Tools.Deps
+{
+    public class TestJsonReporter
+    {
+        [Fact]
+        public void ShouldCloseArray()
+        {
+            using (var m = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(m))
+                {
+                    using (var reporter = new JsonReporter(sw))
+                    {
+
+                    }
+                    sw.Flush();
+                }
+
+                Assert.Equal(@"[
+
+]
+".Replace("\r\n", "\n"), System.Text.Encoding.UTF8.GetString(m.ToArray()).Replace("\r\n","\n"));
+            }
+        }
+        [Fact]
+        public void ShouldShowSortedOrder()
+        {
+            using (var workspace = new AdhocWorkspace())
+            {
+                workspace.AddSolution(SolutionInfo.Create(SolutionId.CreateNewId(),VersionStamp.Create(), "c:\\ws\\app\\app.sln"));
+
+                var p1 = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "A", "A.dll", "C#", "c:\\ws\\app\\src\\A\\A.csproj");
+                var p2 = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "B", "Y.dll", "C#", "c:\\ws\\app\\src\\B\\B.csproj");
+                
+                using (var m = new MemoryStream())
+                {
+                    using (var sw = new StreamWriter(m))
+                    {
+                        using (var reporter = new JsonReporter(sw))
+                        {
+                            var a = workspace.AddProject(p1);
+                            var b = workspace.AddProject(p2);
+                            reporter.Report(a);
+                            reporter.Report(b);
+                        }
+                        sw.Flush();
+                    }
+
+                    Assert.Equal(@"[
+  { ""Name"": ""A"", ""AssemblyName"": ""A.dll"", ""FilePathFromSolutionDir"": ""src\\A\\A.csproj"" },
+  { ""Name"": ""B"", ""AssemblyName"": ""Y.dll"", ""FilePathFromSolutionDir"": ""src\\B\\B.csproj"" }
+]
+".Replace("\r\n", "\n"), System.Text.Encoding.UTF8.GetString(m.ToArray()).Replace("\r\n","\n"));
+                }
+            }
+        }
+    }
+}
